44bd91906379346ee870cc1f3b1092593da2c2d7
César Mejía <cmejia@goplek.com>
2019-04-30 13:24:47 -0500

    Manejo de errores con serviceWorkers y fetch API

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,6 +1,11 @@
 
 self.addEventListener('fetch', event => {
 
-    
+    const resp = fetch(event.request)
+        .then(res => {
+            return res.ok ? res : fetch('/img/main.jpg');
+        });
+
+    event.respondWith( resp );
 
 });
