ca3ffeb6829f6dc5ef10f0d4fdeab278476e2da0
César Mejía <iconfidence@gmail.com>
2019-05-05 12:55:13 -0500

    05 offline navigation

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -10,6 +10,7 @@ const cacheStaticFiles = [
     '/index.html',
     '/css/style.css',
     '/img/main.jpg',
+    '/img/no-img.jpg',
     '/js/app.js',
     '/pages/offline.html'
 ];
@@ -62,8 +63,64 @@ self.addEventListener('install', event => {
 // Handle fetch event of service worker.
 self.addEventListener('fetch', event => { 
     
+    // 5. Cache and Network race
+    const response = new Promise((res, rej) => {
+        let fail = false;
+        const failOnce = () => {
+            if (fail) {
+                if (/\.(png|jpg)$/i.test(event.request.url)) {
+                    res(caches.match('/img/no-img.jpg'));
+                } else {
+                    rej('No se encontró respuesta');
+                }
+            } else {
+                fail = true;
+            }
+        };
+
+        // Try to get the resource from network
+        fetch(event.request)
+            .then(response => {
+                response.ok ? res(response) : failOnce();
+            })
+            .catch(failOnce);
+
+        // Try to get the resource from cache
+        caches.match(event.request)
+            .then(response => {
+                response ? res(response) : failOnce();
+            })
+            .catch(failOnce);
+
+    });
+
+    event.respondWith(response);
+
+
+    // 4. Cache with network update
+    // Se utiliza cuando el rendimiento es crítico
+    /* if (event.request.url.includes('bootstrap')) {
+        return event.respondWith(caches.match(event.request));
+    }
+
+    const response = caches.open(CACHE_STATIC_NAME)
+        .then(cache => {
+
+            // Store resorce possible update in the cache
+            fetch(event.request)
+                .then(res => {
+                    cache.put(event.request, res);
+                });
+
+            // Returns request from cache.
+            return cache.match(event.request);
+        });
+
+
+    event.respondWith( response ); */
+
     // 3. Network with cache fallback
-    const responseNetwork = fetch(event.request)
+    /* const responseNetwork = fetch(event.request)
         .then(res => {
             if (!res) {
                 return caches.match(event.request);
@@ -81,7 +138,7 @@ self.addEventListener('fetch', event => {
             return caches.match(event.request);
         });
 
-    event.respondWith(responseNetwork);
+    event.respondWith(responseNetwork); */
 
 
     // 2. Cache with fallback
